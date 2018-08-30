c1871b401d4c0ebe753db43cb3df0053a8139cce
Sebastian <bgjuan8@misena.edu.co>
2018-08-30 11:01:53 -0500

    Inicio

--- app/src/main/java/com/regionaltolima/worldskills/colorapp/MainActivity.java
+++ app/src/main/java/com/regionaltolima/worldskills/colorapp/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-        int modificar;
-        String modificar2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
